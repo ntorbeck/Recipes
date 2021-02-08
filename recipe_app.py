aae85edb6eef3c1f00cd7ad029f5501c5e807b89
Nyssa <ntorbeck@gmail.com>
2021-02-08 14:26:33 -0600

    Adding cs50 SQL class

--- recipe_app.py
+++ recipe_app.py
@@ -5,6 +5,7 @@ from werkzeug.exceptions import default_exceptions, HTTPException, InternalServe
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from helpers import login_required
+from cs50 import SQL
 
 # Configure application
 app = Flask(__name__)
@@ -25,6 +26,9 @@ app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///recipe.db")
 
 @app.route('/')
 @login_required
