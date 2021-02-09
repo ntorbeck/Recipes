acef93ca2c38c6f842446237f847183c0942fdad
Nyssa <ntorbeck@gmail.com>
2021-02-09 15:28:29 -0600

    adds func: converts list of dicts to dict of dicts

--- helpers.py
+++ helpers.py
@@ -1,10 +1,21 @@
 from flask import redirect, session
 from functools import wraps
 
+def get_dict(id_name, sql_list):
+    """Returns dict of dicts for id/name mapping"""
+    
+    # https://stackoverflow.com/questions/5236296/how-to-convert-list-of-dict-to-dict
+    column_id = id_name
+    sql_dict = dict()
+    for item in sql_list:
+        index = item.pop(column_id)
+        sql_dict[index] = item
+    return sql_dict
+
+
 def login_required(f):
     """
     Decorate routes to require login.
-
     http://flask.pocoo.org/docs/1.0/patterns/viewdecorators/
     """
     @wraps(f)
